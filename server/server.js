a6c8cfc7d0dce1bb39903e59b417aede3f9b33c2
ThomasKWWong <thomaskwwong7@gmail.com>
2024-08-19 22:23:16 -0400

    established connection with google gemini-1.5-flash AI

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+//middleware
+const googleAI = new GoogleGenerativeAI(process.env.API_KEY)
+const model = googleAI.getGenerativeModel({ model : "gemini-1.5-flash"});
+app.use(cors());
+app.use(express.json());
+
+//API endpoints
+app.post("/api/test", async(req, res) => {
+    try{
+        return res.json({status: 'ok'});
+    }
+    catch(err){
+        console.log(err);
+    }
+})
+
+app.post("/api/ai", async (req, res) => {
+    try {
+        const result = await model.generateContent("what's your name?")
+        return res.json({ message: result.response.text() });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+});
+
+
+app.listen(5000, () => {
+    console.log("Server is listening to port 5000")
+})
